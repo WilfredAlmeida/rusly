c9a7f88b0cc6cd01251ae32cb12f5febd666e0bb
WilfredAlmeida <almeidawilfred642@gmail.com>
2023-04-11 13:11:14 +0530

    feat: json response, removed unsued imports

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate rocket;
-use rocket::{serde::{Serialize, Deserialize, json::Json}, Response};
+use rocket::{
+    response::Redirect,
+    serde::{json::Json, Deserialize, Serialize},
+};
 
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection};
 
 use rand::distributions::{Alphanumeric, DistString};
 
@@ -10,86 +13,125 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct RequestBody{
+struct RequestBody {
     url_to_shorten: String,
-    custom_link: Option<String>
+    custom_link: Option<String>,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct ResponseBody{
+struct ResponseBody {
     shortened_url: Option<String>,
-    error: Option<String>
+    error: Option<String>,
 }
 
 static HOST_URI: &str = "http://127.0.0.1:8001";
 
-#[post("/shorten",data = "<request_body>")]
+#[post("/shorten", data = "<request_body>")]
 fn shorten_url_handler(request_body: Json<RequestBody>) -> Json<ResponseBody> {
-
-    let db = match Connection::open("urls.db"){
+    let db = match Connection::open("urls.db") {
         Ok(c) => c,
         Err(e) => {
             println!("1");
             eprintln!("{}", e.to_string());
-            return Json(ResponseBody {shortened_url: None, error: Some(e.to_string())})
+            return Json(ResponseBody {
+                shortened_url: None,
+                error: Some(e.to_string()),
+            });
         }
     };
 
-    match db.execute("CREATE TABLE IF NOT EXISTS urls (
+    match db.execute(
+        "CREATE TABLE IF NOT EXISTS urls (
         id TEXT(7) PRIMARY KEY,
         fullUrl TEXT(512) NOT NULL,
         time INTEGER NOT NULL
-    )",()) {
-        Ok(result) =>{
+    )",
+        (),
+    ) {
+        Ok(result) => {
             println!("{}", result)
-        },
-        Err(err)=>{
+        }
+        Err(err) => {
             println!("2");
-            eprintln!("{}",err.to_string());
-            return Json(ResponseBody {shortened_url: None, error: Some(err.to_string())})
+            eprintln!("{}", err.to_string());
+            return Json(ResponseBody {
+                shortened_url: None,
+                error: Some(err.to_string()),
+            });
         }
     }
 
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 7);
     println!("{}", string.to_lowercase());
 
-    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)
-    .unwrap()
-    .as_secs();
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
 
-    match db.execute("INSERT INTO urls (id,fullUrl,time) VALUES (?1, ?2, ?3)", (&string, &request_body.url_to_shorten, timestamp)){
+    match db.execute(
+        "INSERT INTO urls (id,fullUrl,time) VALUES (?1, ?2, ?3)",
+        (&string, &request_body.url_to_shorten, timestamp),
+    ) {
         Ok(result) => {
             if (result == 1) {
                 println!("Data Inserted");
-                println!("{}",result);
+                println!("{}", result);
 
-                return Json(ResponseBody { shortened_url: Some(format!("{}/{}",HOST_URI, string)), error: None });
-
+                return Json(ResponseBody {
+                    shortened_url: Some(format!("{}/{}", HOST_URI, string)),
+                    error: None,
+                });
             }
-        },
+        }
         Err(err) => {
             println!("Insertion Failed");
             eprintln!("{}", err.to_string());
 
-            return Json(ResponseBody{shortened_url: None, error: Some(err.to_string())});
+            return Json(ResponseBody {
+                shortened_url: None,
+                error: Some(err.to_string()),
+            });
         }
     };
 
-
-    Json(ResponseBody { shortened_url: Some(request_body.url_to_shorten.to_string()), error: None })
+    Json(ResponseBody {
+        shortened_url: Some(request_body.url_to_shorten.to_string()),
+        error: None,
+    })
 }
 
+#[get("/<murl>")]
+fn index(murl: String) -> Option<Redirect> {
+    
+    let db = match Connection::open("urls.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("1");
+            eprintln!("{}", e.to_string());
+            return None;
+        }
+    };
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+    let query = format!("SELECT fullUrl FROM urls WHERE id='{}'",murl);
+    let link_to_redirect_to = match db.query_row(&query, [], |row| {
+        row.get::<_,String>(0)
+    }) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("SELECT ERROR");
+            eprint!("{}", e.to_string());
+            return Some(Redirect::permanent("https://google.com/1234qwer"));
+        }
+    };
+
+    return Some(Redirect::permanent(link_to_redirect_to));
 }
-
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/v1", routes![shorten_url_handler])
+        .mount("/", routes![index])
+        .mount("/v1", routes![shorten_url_handler])
 }
